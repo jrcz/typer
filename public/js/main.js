daf9d98f63244e861272c5a19eee1e9d2b9ec044
Joao Zanetti <joao.zanetti@bb.com.br>
2020-11-26 20:44:19 -0300

    Reiniciar Jogo e outros refactores

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,61 @@
-var frase = $(".frase").text()
-
-$("#quantidade-palavras").text(frase.split(" ").length)
-
 var campo = $(".campoDigitacao")
+var tempoInicial = $("#tempoDigitacao").text()
 
-campo.on("input", function(){
-    $("#contadorPalavras").text(campo.val().split(/\s+/).length-1)
-    $("#contadorCaracteres").text(campo.val().length)
+
+$(function(){
+    atualizaTamanhoFrase()
+    inicializaContadores()
+    inicializaCronometro()
+    $("#botaoReiniciar").click(reiniciaJogo)
 })
 
-var tempoRestante = $("#tempoDigitacao").text()
-campo.one("focus", function(){
-    var cronometro = setInterval(function(){
-        tempoRestante--  
-        $("#tempoDigitacao").text(tempoRestante)  
-        if(tempoRestante < 1){
-            campo.attr("disabled", true)
-            clearInterval(cronometro)
-        }
-    }, 1000)
-})
+
+function atualizaTamanhoFrase(){
+    var frase = $(".frase").text()
+    var tempoInicial = $("#tempoDigitacao").text()
+    
+    $("#quantidade-palavras").text(frase.split(" ").length)
+}
+
+
+function inicializaContadores(){
+    campo.on("input", function(){
+        $("#contadorPalavras").text(campo.val().split(/\s+/).length-1)
+        $("#contadorCaracteres").text(campo.val().length)
+    })
+}
+
+
+
+function inicializaCronometro(){
+    var tempoRestante = $("#tempoDigitacao").text()
+    campo.one("focus", function(){
+        $("#botaoReiniciar").attr("disabled", true)
+        var cronometro = setInterval(function(){
+            tempoRestante--  
+            $("#tempoDigitacao").text(tempoRestante)  
+            if(tempoRestante < 1){
+                campo.attr("disabled", true)
+                clearInterval(cronometro)
+                $("#botaoReiniciar").attr("disabled", false)
+            }
+        }, 1000)
+    })
+    
+}
+
+
+
+function reiniciaJogo(){
+    campo.attr("disabled", false)
+    campo.val("")
+    $("#contadorPalavras").text(0)
+    $("#contadorCaracteres").text(0)
+    $("#tempoDigitacao").text(tempoInicial)
+    inicializaCronometro()
+}
+
+
 
 
 

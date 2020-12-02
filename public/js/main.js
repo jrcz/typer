ffac7eb634fd34d02e94f85d191632180d3eedbd
Joao Zanetti <joao.zanetti@bb.com.br>
2020-12-02 15:11:34 -0300

    customização de estilos

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-var campo = $(".campoDigitacao")
+var campo = $(".campo-digitacao")
 var tempoInicial = $("#tempoDigitacao").text()
 
 
@@ -6,6 +6,7 @@ $(function(){
     atualizaTamanhoFrase()
     inicializaContadores()
     inicializaCronometro()
+    inicializaMarcadores()
     $("#botaoReiniciar").click(reiniciaJogo)
 })
 
@@ -14,7 +15,7 @@ function atualizaTamanhoFrase(){
     var frase = $(".frase").text()
     var tempoInicial = $("#tempoDigitacao").text()
     
-    $("#quantidade-palavras").text(frase.split(" ").length)
+    $("#quantidade-palavras").text(frase.split(/\s+/).length)
 }
 
 
@@ -25,6 +26,21 @@ function inicializaContadores(){
     })
 }
 
+function inicializaMarcadores(){
+    var frase = $(".frase").text()
+    campo.on("input", function(){
+        var digitado = campo.val()
+        var comparavel = frase.substr(0, digitado.length)
+    
+        if(digitado == comparavel){
+            campo.addClass("borda-verde")
+            campo.removeClass("borda-vermelha")
+        }else{
+            campo.addClass("borda-vermelha")
+            campo.removeClass("borda-verde")
+        }
+    })
+}
 
 
 function inicializaCronometro(){
@@ -38,6 +54,7 @@ function inicializaCronometro(){
                 campo.attr("disabled", true)
                 clearInterval(cronometro)
                 $("#botaoReiniciar").attr("disabled", false)
+                campo.addClass("campo-desativado")
             }
         }, 1000)
     })
@@ -52,6 +69,9 @@ function reiniciaJogo(){
     $("#contadorPalavras").text(0)
     $("#contadorCaracteres").text(0)
     $("#tempoDigitacao").text(tempoInicial)
+    campo.removeClass("campo-desativado")
+    campo.removeClass("borda-vermelha")
+    campo.removeClass("borda-verde")
     inicializaCronometro()
 }
 
